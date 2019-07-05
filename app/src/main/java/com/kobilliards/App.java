e4bb76ff42dc32659d7bc2dc621f66fd3ecb6c9c
陈冠希 <chenguanxi@shoujinwang.com>
2019-07-05 18:30:18 +0800

    主页面底部导航栏

--- app/src/main/java/com/kobilliards/App.java
+++ app/src/main/java/com/kobilliards/App.java
@@ -3,6 +3,8 @@ package com.kobilliards;
 import android.app.Activity;
 import android.app.Application;
 import android.os.Bundle;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 import com.kobilliards.base.ActivityManager;
 import com.kobilliards.constants.PreferenceConstant;
@@ -12,19 +14,20 @@ import com.kobilliards.utils.SPUtils;
 import com.kobilliards.utils.log.LogUtil;
 import com.netease.nimlib.sdk.NIMClient;
 
-public class App extends Application implements Application.ActivityLifecycleCallbacks {
+public class App extends MultiDexApplication implements Application.ActivityLifecycleCallbacks {
 
 
 
     @Override
     public void onCreate() {
         super.onCreate();
-        NIMClient.init(this, CommonUtils.getLoginInfo(), null);
 
         AppUtils.init(this);
+        MultiDex.install(this);
         SPUtils.init(PreferenceConstant.PREFERENCE_NAME,this);
         LogUtil.init(BuildConfig.DEBUG);
         registerActivityLifecycleCallbacks(this);
+        NIMClient.init(this, CommonUtils.getLoginInfo(), null);
 
     }
 
